e7f9aedf4572112f7354dcb929362d03a9dd6f0e
MsAnonymous0 <150545090+MsAnonymous0@users.noreply.github.com>
2026-06-23 19:30:01 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-javascript
+const openBtn = document.getElementById("clickopen");
+const openBtn = document.getElementById("clickclose");
+const openBtn = document.getElementById("Modal");
+
+
+openBtn.addEventListener("click", () => {modal.classList.add("open");});
+closeBtn.addEventListener("click", () => {modal.classList.remove("open");});
