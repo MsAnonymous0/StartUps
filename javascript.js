65d2ed023ba8c21af7d6f5ab83cd07edca960c0d
MsAnonymous0 <150545090+MsAnonymous0@users.noreply.github.com>
2026-06-23 20:01:29 -0400

    Remove empty line in javascript.js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,5 @@ const openBtn = document.getElementById("clickopen");
 const closeBtn = document.getElementById("clickclose");
 const modal = document.getElementById("modal");
 
-
 openBtn.addEventListener("click", () => {modal.classList.add("open");});
 closeBtn.addEventListener("click", () => {modal.classList.remove("open");});
